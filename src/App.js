8dbb53346d4d49b3174347a9c6277d1048545e42
Nisha Thapa <nishathapa4727@gmail.com>
2020-07-18 01:44:42 +0530

    Updated fetched data

--- src/App.js
+++ src/App.js
@@ -3,17 +3,21 @@ import React from "react";
 import { Cards, Chart, CountryPicker } from "./components";
 import styles from "./App.module.css";
 import { fetchData } from "./api";
+import axios from "axios";
 
 class App extends React.Component {
+  state = {
+    data: {},
+  };
   async componentDidMount() {
-    const data = await fetchData();
-
-    console.log(data);
+    const fetchedData = await fetchData();
+    this.setState({ data: fetchedData });
   }
   render() {
+    const { data } = this.state;
     return (
       <div className={styles.container}>
-        <Cards />
+        <Cards data={data} />
         <CountryPicker />
         <Chart />
       </div>
